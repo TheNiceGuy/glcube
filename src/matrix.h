57f2965632df2ea7eaaa42a188493892dbb29147
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2014-02-15 00:32:13 -0400

    fixed license

--- src/matrix.h
+++ src/matrix.h
@@ -1,3 +1,20 @@
+/////////////////////////////////////////////////////////////////////////
+//                                                                     //
+//  matrix.h                                                           //
+//                                                                     //
+//  Copyright (c) 2013-2014 Gabriel-Andrew Pollo Guilbert              //
+//                          <gabriel14_wii@hotmail.com>                //
+//                                                                     //
+//                        All Rights Reserved                          //
+//                                                                     //
+//  This program is free software. It comes without  any warranty, to  //
+//  the extent permitted by  applicable law. You can  redistribute it  //
+//  and/or modify it under the terms of the Do What the Fuck You Want  //
+//  to  Public  License, Version 2, as  published by Sam Hocevar. See  //
+//  http://www.wtfpl.net/ for more details.                            //
+//                                                                     //
+/////////////////////////////////////////////////////////////////////////  
+
 #ifndef MATRIX_H
 #define MATRIX_H
 
